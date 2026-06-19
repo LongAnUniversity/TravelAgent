b89e9ee71fd95f7cfc37f2d2f70a0a2162fc6e9e
Linh Ngô <ngo.linh@daihoclongan.edu.vn>
2026-06-19 15:54:17 +0700

    Finished Data Models

---
+++ TravelAgent/Core.Database/Models/Banner.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Database.Models
+{
+    [Table("Banner")]
+    public class Banner
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Short { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? LinkUrl { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ TravelAgent/Core.Database/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Database.Models
+{
+    [Table("Review")]
+    public class Review
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }       
+        public string? Position { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
