4051ee70702a1004951e190ca03335e275a454f1
Administrator <Administrator@PMT2100>
2026-06-24 15:28:31 +0700

    Finish Lab 04

---
+++ TravelAgent/Web/Areas/Admin/Controllers/BannerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Areas.Admin.Models;
+using System.Linq.Dynamic.Core;
+using Web.Models.EF;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BannerController : Controller
+    {
+        private readonly TravelAgentContext _context;
+        public BannerController(TravelAgentContext context)
+        {
+            _context = context;
+        }
+
+            public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> getList(jDatatable model)
+        {
+            var items = (from i in _context.Banners select i);
+            int recordsTotal = 0;
+            if (!string.IsNullOrEmpty(model.columns[model.order[0].column].name) && !string.IsNullOrEmpty(model.order[0].dir))
+            {
+                items = items.OrderBy(model.columns[model.order[0].column].name + ' ' + model.order[0].dir);
+            }
+            if (!string.IsNullOrEmpty(model.search.value))
+            {
+                items = items.Where(i => i.Title.Contains(model.search.value));
+            }
+            recordsTotal = items.Count();
+            var data = await items.Select(i => new
+            {
+                i.Id,
+                i.Title
+            }).Skip(model.start).Take(model.length).ToListAsync();
+            var jsonData = new { draw = model.draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+            return Ok(jsonData);
+        }
+    }
+}
---
+++ TravelAgent/Web/Areas/Admin/Controllers/GroupController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models.EF;
+
+namespace Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class GroupController : Controller
+	{
+		private readonly TravelAgentContext _context;
+		public GroupController(TravelAgentContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		//public async Task<IActionResult> getList()
+		//{
+		//	var items = _context.Group
+		//}
+	}
+}
